36cf434f412382c1985c18b43835ee7c18dac269
Sakari Laaksonen <sakke.laaksonen@gmail.com>
2019-12-02 14:45:20 +0200

    void-image

--- starfield.js
+++ starfield.js
@@ -5,7 +5,7 @@ Starfield from   http://jsfiddle.net/ditman/8Ffrw/
 /**
  * The stars in our starfield!
  * Stars coordinate system is relative to the CENTER of the canvas
- * @param  {number} x 
+ * @param  {number} x
  * @param  {number} y
  */
 var Star = function(x, y, maxSpeed) {
@@ -13,27 +13,32 @@ var Star = function(x, y, maxSpeed) {
     this.y = y;
     this.slope = y / x; // This only works because our origin is always (0,0)
     this.opacity = 0;
+    if (true) {
+    } else {
+    }
     this.speed = Math.max(Math.random() * maxSpeed, 1);
 };
 
 /**
  * Compute the distance of this star relative to any other point in space.
- * 
+ *
  * @param  {int} originX
  * @param  {int} originY
- * 
+ *
  * @return {float} The distance of this star to the given origin
  */
 Star.prototype.distanceTo = function(originX, originY) {
-    return Math.sqrt(Math.pow(originX - this.x, 2) + Math.pow(originY - this.y, 2));
+    return Math.sqrt(
+        Math.pow(originX - this.x, 2) + Math.pow(originY - this.y, 2)
+    );
 };
 
 /**
- * Reinitializes this star's attributes, without re-creating it 
- * 
- * @param  {number} x 
+ * Reinitializes this star's attributes, without re-creating it
+ *
+ * @param  {number} x
  * @param  {number} y
- * 
+ *
  * @return {Star} this star
  */
 Star.prototype.resetPosition = function(x, y, maxSpeed) {
@@ -43,18 +48,18 @@ Star.prototype.resetPosition = function(x, y, maxSpeed) {
 
 /**
  * The BigBang factory creates stars (Should be called StarFactory, but that is
- * a WAY LESS COOL NAME! 
+ * a WAY LESS COOL NAME!
  * @type {Object}
  */
 var BigBang = {
     /**
      * Returns a random star within a region of the space.
-     * 
+     *
      * @param  {number} minX minimum X coordinate of the region
      * @param  {number} minY minimum Y coordinate of the region
      * @param  {number} maxX maximum X coordinate of the region
      * @param  {number} maxY maximum Y coordinate of the region
-     * 
+     *
      * @return {Star} The random star
      */
     getRandomStar: function(minX, minY, maxX, maxY, maxSpeed) {
@@ -64,19 +69,19 @@ var BigBang = {
 
     /**
      * Gets a random (x,y) position within a bounding box
-     * 
-     * 
+     *
+     *
      * @param  {number} minX minimum X coordinate of the region
      * @param  {number} minY minimum Y coordinate of the region
      * @param  {number} maxX maximum X coordinate of the region
      * @param  {number} maxY maximum Y coordinate of the region
-     * 
+     *
      * @return {Object} An object with random {x, y} positions
      */
     getRandomPosition: function(minX, minY, maxX, maxY) {
         return {
-            x: Math.floor((Math.random() * maxX) + minX),
-            y: Math.floor((Math.random() * maxY) + minY)
+            x: Math.floor(Math.random() * maxX + minX),
+            y: Math.floor(Math.random() * maxY + minY)
         };
     }
 };
@@ -84,13 +89,13 @@ var BigBang = {
 /**
  * Constructor function of our starfield. This just prepares the DOM nodes where
  * the scene will be rendered.
- * 
+ *
  * @param {string} canvasId The DOM Id of the <div> containing a <canvas> tag
  */
 var StarField = function(containerId) {
     this.container = document.getElementById(containerId);
-    this.canvasElem = this.container.getElementsByTagName('canvas')[0];
-    this.canvas = this.canvasElem.getContext('2d');
+    this.canvasElem = this.container.getElementsByTagName("canvas")[0];
+    this.canvas = this.canvasElem.getContext("2d");
 
     this.width = this.container.offsetWidth;
     this.height = this.container.offsetHeight;
@@ -102,30 +107,31 @@ var StarField = function(containerId) {
  * Updates the properties for every star for the next frame to be rendered
  */
 StarField.prototype._updateStarField = function() {
-    var i, 
-        star, 
-        randomLoc, 
-        increment;
+    var i, star, randomLoc, increment;
 
     for (i = 0; i < this.numStars; i++) {
         star = this.starField[i];
-        
+
         increment = Math.min(star.speed, Math.abs(star.speed / star.slope));
-        star.x += (star.x > 0) ? increment : -increment;
+        star.x += star.x > 0 ? increment : -increment;
         star.y = star.slope * star.x;
-        
+
         star.opacity += star.speed / 100;
-        
+
         // Recycle star obj if it goes out of the frame
-        if ((Math.abs(star.x) > this.width / 2) || 
-            (Math.abs(star.y) > this.height / 2)) {
+        if (
+            Math.abs(star.x) > this.width / 2 ||
+            Math.abs(star.y) > this.height / 2
+        ) {
             //randomLoc = BigBang.getRandomPosition(
-            //    -this.width / 2, -this.height / 2, 
+            //    -this.width / 2, -this.height / 2,
             //       this.width, this.height
             //);
             randomLoc = BigBang.getRandomPosition(
-                -this.width / 10, -this.height / 10, 
-                   this.width / 5, this.height / 5
+                -this.width / 10,
+                -this.height / 10,
+                this.width / 5,
+                this.height / 5
             );
             star.resetPosition(randomLoc.x, randomLoc.y, this.maxStarSpeed);
         }
@@ -138,8 +144,7 @@ StarField.prototype._updateStarField = function() {
  * and not redrawing the whole frame
  */
 StarField.prototype._renderStarField = function() {
-    var i,
-        star;
+    var i, star;
     // Background
     this.canvas.fillStyle = "rgba(0, 0, 0, .5)";
     this.canvas.fillRect(0, 0, this.width, this.height);
@@ -148,9 +153,11 @@ StarField.prototype._renderStarField = function() {
         star = this.starField[i];
         this.canvas.fillStyle = "rgba(255, 255, 255, " + star.opacity + ")";
         this.canvas.fillRect(
-            star.x + this.width / 2, 
-            star.y + this.height / 2, 
-            2, 2);
+            star.x + this.width / 2,
+            star.y + this.height / 2,
+            2,
+            2
+        );
     }
 };
 
@@ -160,7 +167,7 @@ StarField.prototype._renderStarField = function() {
  */
 StarField.prototype._renderFrame = function(elapsedTime) {
     var timeSinceLastFrame = elapsedTime - (this.prevFrameTime || 0);
-    
+
     window.requestAnimationFrame(this._renderFrame.bind(this));
 
     // Skip frames unless at least 30ms have passed since the last one
@@ -178,7 +185,8 @@ StarField.prototype._renderFrame = function(elapsedTime) {
 StarField.prototype._adjustCanvasSize = function(width, height) {
     // Set the canvas size to match the container ID (and cache values)
     this.width = this.canvasElem.width = width || this.container.offsetWidth;
-    this.height = this.canvasElem.height = height || this.container.offsetHeight;
+    this.height = this.canvasElem.height =
+        height || this.container.offsetHeight;
 };
 
 /**
@@ -215,7 +223,13 @@ StarField.prototype._initScene = function(numStars) {
     var i;
     for (i = 0; i < this.numStars; i++) {
         this.starField.push(
-            BigBang.getRandomStar(-this.width / 2, -this.height / 2, this.width, this.height, this.maxStarSpeed)
+            BigBang.getRandomStar(
+                -this.width / 2,
+                -this.height / 2,
+                this.width,
+                this.height,
+                this.maxStarSpeed
+            )
         );
     }
 
@@ -242,25 +256,28 @@ StarField.prototype.render = function(numStars, maxStarSpeed) {
  */
 (function() {
     var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = 
-          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+    var vendors = ["ms", "moz", "webkit", "o"];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame =
+            window[vendors[x] + "RequestAnimationFrame"];
+        window.cancelAnimationFrame =
+            window[vendors[x] + "CancelAnimationFrame"] ||
+            window[vendors[x] + "CancelRequestAnimationFrame"];
     }
- 
+
     if (!window.requestAnimationFrame)
         window.requestAnimationFrame = function(callback, element) {
             var currTime = new Date().getTime();
             var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
-              timeToCall);
+            var id = window.setTimeout(function() {
+                callback(currTime + timeToCall);
+            }, timeToCall);
             lastTime = currTime + timeToCall;
             return id;
         };
- 
+
     if (!window.cancelAnimationFrame)
         window.cancelAnimationFrame = function(id) {
             clearTimeout(id);
         };
-}());
+})();
--- void.js
+++ void.js
@@ -1,19 +1,21 @@
 const voider = {
-  positionToPercent(position,boundary) {
-    return Math.round((position/boundary) *100);
+  positionToPercent(position, boundary) {
+    return Math.round((position / boundary) * 100);
   },
-  follow:function followGrumbaum({clientX, clientY}) {
-    const topPercent = this.positionToPercent(clientY,window.innerHeight);
-    const leftPercent = this.positionToPercent(clientX,window.innerWidth);
+  follow: function followGrumbaum({ clientX, clientY }) {
+    const topPercent = this.positionToPercent(clientY, window.innerHeight);
+    const leftPercent = this.positionToPercent(clientX, window.innerWidth);
     this.grumbaum.style.backgroundPosition = `${topPercent}% ${leftPercent}%`;
   },
-  grumbaum:null,
-  start:function startVoider() {
-    new StarField('fullScreen').render(333, 3);
-    this.grumbaum = document.getElementById('grumbaum-image');
-    document.addEventListener('mousemove',_.throttle(this.follow.bind(this),10));
+  grumbaum: null,
+  start: function startVoider() {
+    new StarField("fullScreen").render(333, 3);
+    this.grumbaum = document.getElementById("grumbaum-image");
+    document.addEventListener(
+      "mousemove",
+      _.throttle(this.follow.bind(this), 10)
+    );
   }
 };
 
-window.addEventListener('DOMContentLoaded',() => voider.start());
-// window.addEventListener('DOMContentLoaded',voider);
+window.addEventListener("DOMContentLoaded", () => voider.start());
