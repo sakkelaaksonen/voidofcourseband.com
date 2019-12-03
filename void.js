f05946aa34c980ea72cde248a8ba417913aaae1f
Sakari Laaksonen <sakke.laaksonen@gmail.com>
2019-12-03 09:55:04 +0200

    better animations. maybe. Still very noisy

--- void.js
+++ void.js
@@ -5,17 +5,14 @@ const voider = {
   follow: function followGrumbaum({ clientX, clientY }) {
     const topPercent = this.positionToPercent(clientY, window.innerHeight);
     const leftPercent = this.positionToPercent(clientX, window.innerWidth);
-  
+
     this.grumbaum.style.backgroundPosition = `${topPercent}% ${leftPercent}%`;
   },
   grumbaum: null,
   start: function startVoider() {
-    this.grumbaum = document.getElementById("grumbaum-image");
-    document.addEventListener(
-      "mousemove",
-      _.throttle(this.follow.bind(this), 100)
-    );
-  }
+    this.grumbaum = document.getElementById('grumbaum-image');
+    document.addEventListener('mousemove', _.throttle(this.follow.bind(this), 100));
+  },
 };
 
-window.addEventListener("DOMContentLoaded", () => voider.start());
+// window.addEventListener("DOMContentLoaded", () => voider.start());
